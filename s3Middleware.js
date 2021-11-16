043e1a94ed1b2e90eb83f293d259e8ff5f7192d3
Welberty Martins <38479002+welbertymartins@users.noreply.github.com>
2021-11-16 00:25:18 -0300

    feat: subs ' to "

--- s3Middleware.js
+++ s3Middleware.js
@@ -1,21 +1,21 @@
-const AWS = require('aws-sdk')
-const context = require('./context')
+const AWS = require("aws-sdk")
+const context = require("./context")
 
-const region = context.getEnv('AWS_S3_Region')
-const accessKeyId = context.getEnv('AWS_S3_PublicKey')
-const secretAccessKey = context.getEnv('AWS_S3_PrivateKey')
+const region = context.getEnv("AWS_S3_Region")
+const accessKeyId = context.getEnv("AWS_S3_PublicKey")
+const secretAccessKey = context.getEnv("AWS_S3_PrivateKey")
 
 AWS.config.update({ region, accessKeyId, secretAccessKey })
 
 const S3 = new AWS.S3()
-const Bucket = context.getEnv('AWS_S3_Bucket')
+const Bucket = context.getEnv("AWS_S3_Bucket")
 
 const getObjectContent = async(Key) => {
   try 
   {
     const params = { Bucket, Key }
     const data = await S3.getObject(params).promise()
-    const content = data.Body.toString('utf-8')
+    const content = data.Body.toString("utf-8")
     const err = {}
     const ok = true
     return { ok, content, err }
@@ -28,7 +28,7 @@ const getObjectContent = async(Key) => {
   }
 }
 
-const putObjectContent = async(Key, Body, ContentType = 'application/json') => {
+const putObjectContent = async(Key, Body, ContentType = "application/json") => {
   try 
   { 
     await S3.putObject({ Body, Bucket, ContentType, Key }).promise()
