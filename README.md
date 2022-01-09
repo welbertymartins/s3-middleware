14cd44129b8caad799b8bb6d92d7af991ce459b9
Welberty Martins <38479002+welbertymartins@users.noreply.github.com>
2022-01-09 14:44:52 -0300

    feat: v1.0.1

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# node-14-s3-middleware
-This is an simple S3 middleware project to use in other projects.
+# s3-middleware
+This is an simple S3 middleware to use in other projects.
--- s3Middleware.js
+++ s3Middleware.js
@@ -1,34 +1,34 @@
-const AWS = require("aws-sdk")
-const lambdaMiddleware = require("./lambdaMiddleware")
+const AWS = require('aws-sdk')
+const lambdaMiddleware = require('wam-lambda-middleware')
 
-const region = lambdaMiddleware.getEnv("AWS_S3_Region")
-const accessKeyId = lambdaMiddleware.getEnv("AWS_S3_PublicKey")
-const secretAccessKey = lambdaMiddleware.getEnv("AWS_S3_PrivateKey")
+const region = lambdaMiddleware.getEnv('AWS_S3_Region')
+const accessKeyId = lambdaMiddleware.getEnv('AWS_S3_PublicKey')
+const secretAccessKey = lambdaMiddleware.getEnv('AWS_S3_PrivateKey')
 
 AWS.config.update({ region, accessKeyId, secretAccessKey })
 
 const S3 = new AWS.S3()
-const Bucket = lambdaMiddleware.getEnv("AWS_S3_Bucket")
+const Bucket = lambdaMiddleware.getEnv('AWS_S3_Bucket')
 
 const getObjectContent = async(Key) => {
   try 
   {
     const params = { Bucket, Key }
     const data = await S3.getObject(params).promise()
-    const content = data.Body.toString("utf-8")
+    const content = data.Body.toString('utf-8')
     const err = {}
     const ok = true
     return { ok, content, err }
   } 
   catch (err) 
   {
-    const content = ""
+    const content = ''
     const ok = false
     return { ok, content, err }
   }
 }
 
-const putObjectContent = async(Key, Body, ContentType = "application/json") => {
+const putObjectContent = async(Key, Body, ContentType = 'application/json') => {
   try 
   { 
     await S3.putObject({ Body, Bucket, ContentType, Key }).promise()
